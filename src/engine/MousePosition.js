c1732b35d322b7a776632661ef359d577baad68a
Matthew 'siD' Van der Bijl <matthewvdbijl@gmail.com>
2019-07-04 16:11:58 +0200

    idmypos

---
+++ src/engine/MousePosition.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+/**
+ * idmypos.
+ */
+class MousePosition extends React.Component {
+
+    render() {
+        const { position } = this.props;
+
+        return (
+            <div style={{
+                position: 'absolute',
+                left: '0px',
+                top: '0px'
+            }} className="debug">
+                x={position.x}, y={position.y}
+            </div>
+        );
+    }
+}
+
+export default MousePosition;
