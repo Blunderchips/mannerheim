9f6a76f4a2e9530774af86d5f6aa773c7b4c5133
Matthew 'siD' Van der Bijl <matthewvdbijl@gmail.com>
2019-07-06 18:50:28 +0200

    Moving mob

--- src/engine/Stage.js
+++ src/engine/Stage.js
@@ -1,34 +1,49 @@
 import React from 'react';
 import Tower from './Tower';
 import Board from './Board';
+import Mob from './Mob';
+
+const STEP = 1 / 60;
 
 /**
  * 
  */
 class Stage extends React.Component {
 
+    lastUpdate = 0;
+
     constructor(props) {
         super(props);
         this.state = {
-            towers: []
+            towers: [],
+            mobs: [{ x: 200, y: 200 }]
         };
 
         this.add = this.add.bind(this);
+        this.updadte = this.update.bind(this);
     }
 
     render() {
         return (
             <div onClick={this.add}
                 style={{
-                    background: 'red',
                     width: 800,
                     height: 450
                 }}>
-                {
-                    this.state.towers.map((position, i) => {
-                        return <Tower key={i} position={position} />
-                    })
-                }
+                <div>
+                    {
+                        this.state.towers.map((position, i) => {
+                            return <Tower key={i} position={position} />
+                        })
+                    }
+                </div> 
+                <div>
+                    {
+                        this.state.mobs.map((position, i) => {
+                            return <Mob key={i} position={position} />
+                        })
+                    }
+                </div>
                 <Board />
             </div>
         );
@@ -39,6 +54,28 @@ class Stage extends React.Component {
         _towers.push(this.props.position);
 
         this.setState({ towers: _towers });
+    }
+
+    update() {
+        let current = window.performance.now();
+        let diff = current - this.lastUpdate;
+
+        if (diff >= STEP) {
+            this.lastUpdate = current;
+
+            let _mobs = this.state.mobs;
+            _mobs[0].x = (_mobs[0].x + 1) % 800;
+            // _mobs[0].y = (_mobs[0].y + 1) % 300;
+
+            this.setState({ mobs: _mobs });
+        }
+    }
+
+    componentDidMount() {
+        this.interval = setInterval(() => this.updadte(), 1000 / 60);
+    }
+    componentWillUnmount() {
+        clearInterval(this.interval);
     }
 }
 
