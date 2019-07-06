6d447bea79cce317aa10f2776e32ad5ebe09afc2
Matthew 'siD' Van der Bijl <matthewvdbijl@gmail.com>
2019-07-06 20:21:45 +0200

    Remove not found page
    Not being used

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Particles from 'react-particles-js';
 
 import Game from './engine/Game.js';
