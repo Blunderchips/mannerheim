32848934bfa78c98dce1e379a005f15cdd6b0885
Matthew 'siD' Van der Bijl <matthewvdbijl@gmail.com>
2019-07-19 21:56:47 +0200

    Create Util class

--- src/Util.ts
+++ src/Util.ts
@@ -1,1 +1,31 @@
 export const KEY_SPACE = 32;
+
+/**
+ * 
+ */
+export interface Vector2 {
+
+    x: number;
+    y: number;
+}
+
+/**
+ * 
+ */
+class Util {
+
+    /**
+     * 
+     */
+    static dist2(vecA: Vector2, vecB: Vector2): number {
+
+        // console.log({ vecA, vecB });
+
+        const x = vecA.x - vecB.x;
+        const y = vecA.y - vecB.y;
+
+        return (x * x) + (y * y);
+    }
+}
+
+export default Util;
